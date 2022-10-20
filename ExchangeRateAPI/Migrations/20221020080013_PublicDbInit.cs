8a5b4bf1c1351ba94bdbb8c7e2439480ea68a181
Zygiel <zygiel93@gmail.com>
2022-10-20 10:47:54 +0200

    Update README.md

---
+++ ExchangeRateAPI/Migrations/20221020080013_PublicDbInit.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExchangeRateAPI.Migrations
+{
+    public partial class PublicDbInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
